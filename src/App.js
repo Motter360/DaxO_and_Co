db39c5940ad08d863904a85752d724ccdf8f49bc
Thomas Hammon <hammonthomas@gmail.com>
2024-10-17 14:59:14 -0500

    added img assets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TalkingHead from "./Components/TalkingHead"
 
 function App() {
   return (
     <div className="App">
-      <h1 className="font-bold">Hello World!</h1>
+      <TalkingHead />
     </div>
   );
 }
---
+++ src/Components/TalkingHead.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TalkingHead(){
+    return(
+        <div>
+            <img alt="A robot king sits upon his throne" src="../public/images/false-king.png"/>
+        </div>
+    )
+}
